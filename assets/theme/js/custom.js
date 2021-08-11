00030e4624079ed5adccbc9800a2dfe7b456c082
Michael <chimzeboi07@gmail.com>
2021-08-11 19:29:34 +0100

    test phase

---
+++ assets/theme/js/custom.js
@@ -0,0 +1,36 @@
+const checkbox = document.getElementById('checkbox');
+ 
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+});
+
+// checkbox.addEventListener('click', () => {
+
+//     darkMode = localStorage.getItem('darkMode');
+
+//     if(darkMode !=="enabled"){
+//         enableDarkMode();
+//     } else {
+//         disableDarkMode();
+//     }
+// });
+
+// let darkMode = localStorage.getItem('darkMode');
+
+// const enableDarkMode = () => {
+
+//     document.body.classList.add('dark');
+
+//     localStorage.setItem("darkMode", "enabled");
+// }
+
+// const disableDarkMode = () => {
+//     localStorage.setItem("darkMode", null);
+
+//     document.body.classList.remove('dark');
+
+// }
+
+// if(darkMode === 'enabled'){
+//     enableDarkMode(); 
+// }
